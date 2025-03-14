b3e336e495931ca4173546cf5d3c557af2709fb1
zep <zepothy@gmail.com>
2025-03-13 21:49:56 -0700

    cd into working dir

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -e
+cd /home/chorebot/code/chore-wars-clone/
 git pull
 docker build -t chore-wars:$(date +%s) -t chore-wars:latest .
 docker ps --filter "name=chore-wars" --quiet | grep -q . && docker stop chore-wars
