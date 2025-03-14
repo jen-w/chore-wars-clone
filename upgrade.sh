64caca9a1812c8ea525743edd63c0983a51ade80
zep <zepothy@gmail.com>
2025-03-13 21:15:13 -0700

    updates

--- upgrade.sh
+++ upgrade.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -e
 docker build -t chore-wars:$(date +%s) -t chore-wars:latest .
-docker stop chore-wars
+docker ps --filter "name=chore-wars" --quiet | grep -q . && docker stop chore-wars
 source run.sh
