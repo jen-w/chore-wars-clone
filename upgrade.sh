5b2b1a1c70edf11d3b44378091bdbbf0bfc588ef
zepothy <zepothy@gmail.com>
2025-03-13 21:34:01 -0700

    no bash avaialble

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/sh -e
 git pull
 docker build -t chore-wars:$(date +%s) -t chore-wars:latest .
 docker ps --filter "name=chore-wars" --quiet | grep -q . && docker stop chore-wars
