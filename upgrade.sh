91f2d229d6de32747cf742be5544305e9157910f
zep <zepothy@gmail.com>
2025-03-13 21:22:34 -0700

    add git pull

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
+git pull
 docker build -t chore-wars:$(date +%s) -t chore-wars:latest .
 docker ps --filter "name=chore-wars" --quiet | grep -q . && docker stop chore-wars
 source run.sh
